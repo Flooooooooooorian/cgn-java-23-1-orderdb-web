960293e6f0a8edb23bbb9f829fe3e4c286493a8a
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-02-07 12:50:14 +0100

    Add basic Unittests

---
+++ src/test/java/de/neuefische/mucjava222orderdbweb/service/ShopServiceTest.java
@@ -0,0 +1,51 @@
+package de.neuefische.mucjava222orderdbweb.service;
+
+import de.neuefische.mucjava222orderdbweb.model.Product;
+import de.neuefische.mucjava222orderdbweb.repository.OrderRepository;
+import de.neuefische.mucjava222orderdbweb.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ShopServiceTest {
+
+    @Test
+    void listProducts() {
+        //GIVEN
+        OrderRepository orderRepository = mock(OrderRepository.class);
+        ProductRepository productRepository = mock(ProductRepository.class);
+
+        ShopService shopService = new ShopService(orderRepository, productRepository);
+
+        when(productRepository.list()).thenReturn(List.of(new Product("Product1", "1")));
+
+        //WHEN
+        List<Product> actual = shopService.listProducts();
+
+        //THEN
+        Assertions.assertEquals(List.of(new Product("Product1", "1")), actual);
+    }
+
+    @Test
+    void addProduct() {
+        //GIVEN
+        OrderRepository orderRepository = mock(OrderRepository.class);
+        ProductRepository productRepository = mock(ProductRepository.class);
+
+        ShopService shopService = new ShopService(orderRepository, productRepository);
+
+        when(productRepository.add(new Product("Product2", "2")))
+                .thenReturn(new Product("Product2", "2"));
+
+        //WHEN
+        Product actual = shopService.addProduct(new Product("Product2", "2"));
+
+        //THEN
+        verify(productRepository).add(new Product("Product2", "2"));
+        Assertions.assertEquals(new Product("Product2", "2"), actual);
+    }
+}
